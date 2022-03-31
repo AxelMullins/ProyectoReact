01f61037bb2083f792283eaf73cacc33ff1c5f96
Axel Mullins <axelmullins94@gmail.com>
2022-03-31 18:51:09 -0300

    Carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,18 @@
+
+const Carousel = () => {
+  return (
+    <div>
+        <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Carousel
